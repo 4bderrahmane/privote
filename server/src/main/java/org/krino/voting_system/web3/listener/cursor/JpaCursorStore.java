cd88914f679d5525da470ba2cd8e0d3fa0701de9
Abderrahmane <khbabez7@gmail.com>
2026-03-18 10:12:53 +0000

    Web3EventCursor to ChainSyncCursor

--- server/src/main/java/org/krino/voting_system/web3/listener/cursor/JpaCursorStore.java
+++ server/src/main/java/org/krino/voting_system/web3/listener/cursor/JpaCursorStore.java
@@ -1,7 +1,7 @@
 package org.krino.voting_system.web3.listener.cursor;
 
 import lombok.RequiredArgsConstructor;
-import org.krino.voting_system.entity.Web3EventCursor;
+import org.krino.voting_system.entity.ChainSyncCursor;
 import org.krino.voting_system.repository.Web3EventCursorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,18 +20,18 @@ public class JpaCursorStore implements CursorStore
     public Optional<Cursor> load(String key)
     {
         return repo.findById(key)
-                .map(e -> new Cursor(e.getNextBlock(), e.getNextLogIndex()));
+                .map(e -> new Cursor(e.getLastProcessedBlock(), e.getLastProcessedLogIndex()));
     }
 
     @Override
     @Transactional
     public void save(String key, BigInteger nextBlock, BigInteger nextLogIndex)
     {
-        Web3EventCursor entity = repo.findById(key).orElseGet(Web3EventCursor::new);
+        ChainSyncCursor entity = repo.findById(key).orElseGet(ChainSyncCursor::new);
 
-        entity.setKey(key);
-        entity.setNextBlock(nextBlock);
-        entity.setNextLogIndex(nextLogIndex);
+        entity.setStreamKey(key);
+        entity.setLastProcessedBlock(nextBlock);
+        entity.setLastProcessedLogIndex(nextLogIndex);
 
         repo.save(entity);
     }
