45a2df24a32ef0deea1c828a44cd92e22fc81fba
Abderrahmane <khbabez7@gmail.com>
2026-03-18 09:12:58 +0000

    Party Entity

--- server/src/main/java/org/krino/voting_system/entity/Party.java
+++ server/src/main/java/org/krino/voting_system/entity/Party.java
@@ -6,21 +6,50 @@ import lombok.*;
 import java.util.UUID;
 
 @Entity
+@Table(
+        name = "parties",
+        indexes = {
+                @Index(name = "idx_parties_public_id", columnList = "public_id", unique = true)
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "parties")
 public class Party
 {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, updatable = false)
-    private UUID publicId = UUID.randomUUID();
+    @Column(name = "public_id", nullable = false, unique = true, updatable = false)
+    private UUID publicId;
 
+    @PrePersist
+    void prePersist()
+    {
+        if (publicId == null) publicId = UUID.randomUUID();
+    }
+
+    @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Party party = (Party) o;
+        return id != null && id.equals(party.id);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getClass().hashCode();
+    }
 }
