ebc57c6e02bb6f74a2890c8011563326d38cb36b
Abderrahmane <khbabez7@gmail.com>
2026-03-18 07:48:46 +0000

    Election Patch DTO class

---
+++ server/src/main/java/org/krino/voting_system/dto/election/ElectionPatchDto.java
@@ -0,0 +1,22 @@
+package org.krino.voting_system.dto.election;
+
+import lombok.Data;
+import org.krino.voting_system.entity.enums.ElectionPhase;
+
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+public class ElectionPatchDto
+{
+    private String title;
+    private String description;
+    private Instant startTime;
+    private Instant endTime;
+    private ElectionPhase phase;
+    private BigInteger externalNullifier;
+    private UUID coordinatorKeycloakId;
+    private byte[] encryptionPublicKey;
+    private byte[] decryptionKey;
+}
