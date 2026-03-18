9ff44deafc169fc351c47865320dc20584fd3b28
Abderrahmane <khbabez7@gmail.com>
2026-03-18 09:29:21 +0000

    Candidate Repository

--- server/src/main/java/org/krino/voting_system/repository/CandidateRepository.java
+++ server/src/main/java/org/krino/voting_system/repository/CandidateRepository.java
@@ -4,11 +4,18 @@ import org.krino.voting_system.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long>
 {
-    Optional<Candidate> findCandidateByCitizenKeycloakId(UUID citizenId);
+    Optional<Candidate> findByPublicId(UUID publicId);
+
+    List<Candidate> findByElectionPublicId(UUID electionPublicId);
+
+    boolean existsByElectionPublicIdAndCitizenKeycloakId(UUID electionPublicId, UUID citizenPublicId);
+
+    boolean existsByElectionPublicIdAndCitizenKeycloakIdAndPublicIdNot(UUID electionPublicId, UUID citizenPublicId, UUID publicId);
 }
