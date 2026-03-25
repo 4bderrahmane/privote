e7e5e501c65e65c3ac7e2271e02f399b2ac1d154
Abderrahmane <khbabez7@gmail.com>
2026-03-25 17:25:04 +0100

    code refactor

--- server/src/main/java/org/privote/backend/controller/CitizenController.java
+++ server/src/main/java/org/privote/backend/controller/CitizenController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.privote.backend.entity.Citizen;
 import org.privote.backend.service.CitizenService;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
